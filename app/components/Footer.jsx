ee538eccff110a8f00bff413f7d1789864612c41
Zetaii <zerga556@yahoo.com>
2024-05-07 11:08:08 -0500

    Created Footer
    Footer created, styling not finished

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-900 h-20 mx-auto text-center justify-center ">
+      <div className="border-t-2 border-black">
+        <div className="pt-14">© 2024 Our Website. All rights reserved.</div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
